b81825eb66bd5a9dac6a1e3ff4dfb99e6addd5ac
Zu Santu Nfimmeri <zusantu@dyne.org>
2015-12-11 17:06:37 +0000

    add ex0 template

---
+++ ch3/ex0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# To be used as an exercise template for all the rest of the exercises.
+#
+# Style guide to be used: https://google.github.io/styleguide/pyguide.html
+#
+
+def main():
+    print "Hello world!"
+    return
+
+if __name__ == '__main__':
+    main()
