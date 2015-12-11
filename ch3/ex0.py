ae881b520a2240624928ede64d90e4d70845437a
Zu Santu Nfimmeri <zusantu@dyne.org>
2015-12-11 17:11:02 +0000

    clearer explanations

--- ch3/ex0.py
+++ ch3/ex0.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
-#
+# *** SKIP THIS *** 
 # To be used as an exercise template for all the rest of the exercises.
 #
 # Style guide to be used: https://google.github.io/styleguide/pyguide.html
+#
+# EX <number>: exercise text
+#
+# Example:
+# *** STOP SKIPPING HERE ***
+#
+# EX 0: print hello world.
 #
 
 def main():
