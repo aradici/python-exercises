37a3a388c32d5f5ddbcb8bbb416926cad7b4ef07
zioarazio <zioarazio@users.noreply.github.com>
2015-12-12 12:16:57 +0000

    fixed empty lines again and xrange

--- ch3/ex01.py
+++ ch3/ex01.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # EX 01: create a new list with 3 random strings as elements
+
 import random
 
 def main():
-
 	lista = []
-
-	for _ in range(3):
+	for _ in xrange(3):
 		lista.append(random.choice('abcdefghijklmnopqrstuvwxyz'))
-
 	print lista
 
 if __name__ == '__main__':
