b5096eb25e37d2b97bc5b2b26f9f0a8f0ec15427
zioarazio <zioarazio@users.noreply.github.com>
2015-12-12 12:10:02 +0000

    fixed empty lines and comments

--- ch3/ex01.py
+++ ch3/ex01.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-
 # EX 01: create a new list with 3 random strings as elements
-
 import random
 
 def main():
 
 	lista = []
-	# I begin creating an empty list called lista
-	
-	# I decided to use a for cycle to generate the new list.
-	# it was used _ instead of x so _ will not memorize the return of range
+
 	for _ in range(3):
 		lista.append(random.choice('abcdefghijklmnopqrstuvwxyz'))
 
