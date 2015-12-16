6392f3d34f764bbd9f2dda7a37ca27926f4a84d0
zioarazio <zioarazio@users.noreply.github.com>
2015-12-16 16:42:10 +0000

    fixed print() and I made string creation a bit more interesting

--- ch3/ex01.py
+++ ch3/ex01.py
@@ -2,16 +2,20 @@
 # EX 01: create a new list with 3 random strings as elements
 
 import random
+import string
+
+def rndstrng():
+    stringarandom = ''
+    for _ in xrange(10):
+        stringarandom += random.choice(string.ascii_letters+string.digits)
+    return stringarandom
 
 def main():
-	lista = []
-	for _ in xrange(3):
-		lista.append(random.choice('abcdefghijklmnopqrstuvwxyz'))
-	print lista
+    lista = []
+    for _ in xrange(3):
+            stringa = rndstrng()
+            lista.append(stringa)
+    print (lista)
 
 if __name__ == '__main__':
     main()
-
-
-
-
